f5a3e2a48a17f4dc38f2233113bbaf2cc78fce3d
vc <liam.graves.2016@live.rhul.ac.uk>
2017-07-14 13:55:31 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,70 @@
+setTimeout(function(){
+        $(".element0").typed({
+            strings: ["confirmpresense --user 'strnks'"],
+            typeSpeed: 30, // typing speed
+            backDelay: 750, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        });
+    }, 0);
+
+setTimeout(function(){
+        $(".element1").css("display", "inherit");
+        $(".element1").typed({
+            strings: ["(•_•)"],
+            typeSpeed: 20, // typing speed
+            backDelay: 750, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        });
+    }, 2000);
+
+setTimeout(function(){
+      $(".element2").css("display", "inherit");
+      $(".element2").typed({
+          strings: ["<) )╯"],
+          typeSpeed: 30, // typing speed
+          backDelay: 750, // pause before backspacing
+          loop: false, // loop on or off (true or false)
+          loopCount: false, // number of loops, false = infinite
+          callback: function(){ } // call function after typing is done
+      });
+  }, 2500);
+
+setTimeout(function(){
+        $(".element3").css("display", "inherit");
+        $(".element3").typed({
+            strings: ["/ \\"],
+            typeSpeed: 30, // typing speed
+            backDelay: 750, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        });
+    }, 3200);
+
+setTimeout(function(){
+        $(".element4").css("display", "inherit");
+        $(".element4").typed({
+            strings: ["Yup"],
+            typeSpeed: 30, // typing speed
+            backDelay: 750, // pause before backspacing
+            loop: false, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        });
+    }, 3900);
+
+setTimeout(function(){
+        $(".element5").css("display", "inherit");
+        $(".element5").typed({
+            strings: ["_"],
+            typeSpeed: 30, // typing speed
+            backDelay: 30, // pause before backspacing
+            loop: true, // loop on or off (true or false)
+            loopCount: false, // number of loops, false = infinite
+            callback: function(){ } // call function after typing is done
+        });
+    }, 4500);
